257eec5773905e2d6df1e2a9c2186d32f918c615
Ewerton Filipe <xewertonfilipe@gmail.com>
2021-09-10 11:18:43 -0300

    feat: lesson nine

---
+++ Antes-do-Framework/09-Expressões/script.js
@@ -0,0 +1,14 @@
+const groupA = 100;
+const groupB = 300;
+const winner = groupA > groupB ? "Grupo A Venceu" : "Grupo B Venceu";
+
+const number = [2, 3, 4, 5, 6];
+const total = number.filter(numero => numero > 4);
+
+const active = true;
+const button = active && "Botão está ativo";
+
+const groupWinner = groupA > 90 && "Grupo A Venceu";
+
+console.log(groupWinner);
+
