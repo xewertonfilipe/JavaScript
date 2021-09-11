3786c7306fdcd2d02615b4f7214e729f3e77a957
Ewerton Filipe <xewertonfilipe@gmail.com>
2021-09-11 15:43:21 -0300

    feat: lesson ten

--- Antes-do-Framework/10-Class/script.js
+++ Antes-do-Framework/10-Class/script.js
@@ -1,56 +1,56 @@
 class SmoothScroll {
     constructor(links, menu) {
-      this.linkElements = document.querySelectorAll(links);
-      this.menuElement = document.querySelector(menu);
+      this.linkElements = document.querySelectorAll(links)
+      this.menuElement = document.querySelector(menu)
   
       // Bind linka o this relacionado a classe SmoothScroll,
       // permanentemente ao handleClick
-      this.handleClick = this.handleClick.bind(this);
-      this.addScrollEvent();
+      this.handleClick = this.handleClick.bind(this)
+      this.addScrollEvent()
     }
     
     handleClick(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
-      const section = document.querySelector(href);
+      event.preventDefault()
+      const href = event.currentTarget.getAttribute("href")
+      const section = document.querySelector(href)
       window.scrollTo({
         top: section.offsetTop - (window.innerHeight - section.clientHeight) / 2,
         behavior: "smooth"
-      });
+      })
     }
 
     addScrollEvent() {
       this.linkElements.forEach(link =>
         link.addEventListener("click", this.handleClick)
-      );
+      )
     }
   }
 
   class ActiveSmoothScroll extends SmoothScroll {
     constructor(links, menu, sections) {
       // O super vai ativar o construtor da classe extendida
-      super(links, menu);
-      this.sectionElements = document.querySelectorAll(sections);
+      super(links, menu)
+      this.sectionElements = document.querySelectorAll(sections)
   
-      this.handleScroll = this.handleScroll.bind(this);
-      this.handleScroll();
-      this.activeNavScroll();
+      this.handleScroll = this.handleScroll.bind(this)
+      this.handleScroll()
+      this.activeNavScroll()
     }
 
     handleScroll() {
       this.sectionElements.forEach((section, i) => {
         if (window.pageYOffset > section.offsetTop - window.innerHeight * 0.5) {
-          this.linkElements[i].classList.add("active");
+          this.linkElements[i].classList.add("active")
         } else {
-          this.linkElements[i].classList.remove("active");
+          this.linkElements[i].classList.remove("active")
         }
-      });
+      })
     }
 
     activeNavScroll() {
-      window.addEventListener("scroll", this.handleScroll);
+      window.addEventListener("scroll", this.handleScroll)
     }
   }
   
-  new ActiveSmoothScroll("a[href^='#']", ".menu", "section");
+  new ActiveSmoothScroll("a[href^='#']", ".menu", "section")
   
