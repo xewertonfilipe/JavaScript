5e539074d6c93f821652aa6947f97d98ce581342
Ewerton Filipe <xewertonfilipe@gmail.com>
2021-09-11 15:44:34 -0300

    feat: lesson three

--- Antes-do-Framework/03-Destructuring/script.js
+++ Antes-do-Framework/03-Destructuring/script.js
@@ -1,36 +1,36 @@
 function handleMouseMove(event) {
-    const clientX = event.clientX;
-    const clientY = event.clientY;
-    console.log(clientX, clientY);
+    const clientX = event.clientX
+    const clientY = event.clientY
+    console.log(clientX, clientY)
   }
   
   // Define uma constante para cada propriedade
   // dentro de event, que tenha o mesmo nome que a constante.
   function handleMouseMove(event) {
-    const { clientX, clientY } = event;
-    console.log(clientX, clientY);
+    const { clientX, clientY } = event
+    console.log(clientX, clientY)
   }
   
   // Podemos desestruturar o parâmetro
   function handleMouseMove( { clientX, clientY } ) {
-    console.log(clientX, clientY);
+    console.log(clientX, clientY)
   }
 
 // Desestruturação em array
-const fruits = ["Banana", "Uva"];
-const [fruits1, fruits2] = fruits;
+const fruits = ["Banana", "Uva"]
+const [fruits1, fruits2] = fruits
 
-document.documentElement.addEventListener("mousemove", handleMouseMove);
+document.documentElement.addEventListener("mousemove", handleMouseMove)
 
 
 //React hooks
 const useState = [
   "blue",
   function(color) {
-    useState[0] = color;
+    useState[0] = color
   }
-];
+]
 
-const [color, setColor] = useState;
+const [color, setColor] = useState
 
-setColor("Red");
+setColor("Red")
