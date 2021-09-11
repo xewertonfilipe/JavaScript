0380744e58ba5b06f487b85468ceb13481267bd0
Ewerton Filipe <xewertonfilipe@gmail.com>
2021-09-11 15:47:11 -0300

    feat: lesson two

--- Antes-do-Framework/02-Arrow-Function/script.js
+++ Antes-do-Framework/02-Arrow-Function/script.js
@@ -1,27 +1,27 @@
 const upperName = function(name) {
-    return name.toUpperCase();
-  };
-  upperName("Ewerton"); // EWERTON
+    return name.toUpperCase()
+  }
+  upperName("Ewerton") // EWERTON
   
-  const lowerName = name => name.toLowerCase();
-  lowerName("Filipe"); // Filipe
+  const lowerName = name => name.toLowerCase()
+  lowerName("Filipe") // Filipe
   
-  const countLetters = word => word.length;
-  countLetters("Filipe"); // 6
+  const countLetters = word => word.length
+  countLetters("Filipe") // 6
   
 
 class Menu {
     constructor(menu) {
-        this.menuElement = document.querySelector(menu);
-        this.activeClass = "active";
+        this.menuElement = document.querySelector(menu)
+        this.activeClass = "active"
     }
 
     addActiveEvent() {
         this.menuElement.addEventListener("click", event => {
-            event.target.classList.add(this.activeClass);
-        });
+            event.target.classList.add(this.activeClass)
+        })
     }
 }
 
-const menu = new Menu(".principal");
-menu.addActiveEvent();
+const menu = new Menu(".principal")
+menu.addActiveEvent()
